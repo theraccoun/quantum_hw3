bda803fd37172839f75ba2ca60377c3781422f41
Steven MacCoun <theraccoun@gmail.com>
2012-11-12 00:54:24 -0700

    Refactored commented text

--- simonsProblem.py
+++ simonsProblem.py
@@ -14,20 +14,15 @@ port = 443
 def is_linearly_independent(mat):
 	if len(mat) == 0:
 		return True
-	# print "--------------------------------------"
-	# print "ar: \n" , array(mat)
+
 	cur_row = len(mat) - 1
 
 	for col in range(len(mat[cur_row])):
-		# print "cur_matrix[",cur_row,'][',col,'] = ' , mat[cur_row][col]
-		# raw_input()
-		# print cur_row, mat[cur_row]
 		if mat[cur_row][col] == 1:
 			for look_up in range(1, cur_row + 2):
 
 				examine_row_for_piv = cur_row-look_up
 				if cur_row - look_up < 0:
-					# print "cur_row=" , cur_row
 					return True
 				if mat[examine_row_for_piv][col] == 1:
 
@@ -38,23 +33,6 @@ def is_linearly_independent(mat):
 
 							examine_row_value =  mat[examine_row_for_piv][c]
 							cur_row_value = mat[cur_row][c]
-							# print examine_row_value
-							# print "c=" , c, " len=" , len(mat[cur_row])-1
-							# raw_input()
-
-							# if c == len(mat[cur_row])-1:
-							# 	examine_row_value = int(examine_row_value.replace("|",""))
-							# 	cur_row_value = int(cur_row_value.replace("|",""))
-							# 	xored = cur_row_value ^ examine_row_value
-							# 	print cur_row_value, examine_row_value, xored
-							# 	print "examine: " , mat[examine_row_for_piv]
-							# 	print "cur: " , mat[cur_row]
-							# 	raw_input()
-							# 	new_row.append("|" + str(xored))
-							# 	print xored
-							# 	print "NEW: " , new_row
-							# else:
-							# 	new_row.append(cur_row_value ^ examine_row_value)
 							new_row.append(cur_row_value ^ examine_row_value)
 
 
@@ -69,22 +47,6 @@ def row_contains_earlier_pivot(examine_row, col, mat):
 		for i in range(1, col+1):
 			if mat[examine_row][col-i] == 1:
 				return False
-	return True
-
-	# for col in range(len(cur_matrix[cur_row])):
-	# 	print "cur_matrix[",cur_row,'][',col,'] = ' , cur_matrix[cur_row][col]
-	# 	raw_input()
-
-	# 	if cur_matrix[cur_row][col] == 0:
-	# 		print "0!"
-	# 		continue
-	# 	else:
-	# 		print "meow"
-	# for r in range(len(cur_matrix)):
-	# 	for c in range(r+1):
-	# 		print "cur_matrix[",r,'][',c,'] = ' , cur_matrix[r][c]
-	# 		raw_input()
-
 	return True
 
 def make_lower_triangular(my_array):
@@ -107,7 +69,6 @@ def make_lower_triangular(my_array):
 def get_all_linear_independents(mat_size, from_server):
 	first_row = [0 for z in range(mat_size-1)]
 	first_row.append(1)
-	# first_row.append("|1")
 	mat = [first_row]
 	y = ""
 	trials = 0
@@ -124,24 +85,15 @@ def get_all_linear_independents(mat_size, from_server):
 		y = [int(bit) for bit in y]
 		if len(y) != mat_size:
 			continue
-		# y.append("|0")
 
 		mat.append(y)
-		# print len(mat)
-		# print mat
+		
 		if not is_linearly_independent(mat):
 			mat.pop()
 
 		trials += 1
-		if trials%50 == 0:
-			print "TRIALS=" , trials
 
 		y = ""
-
-	# print "FINAL ANS: \n" , array(mat)
-	# print "------------------------------------"
-	# print "TRIALS: " , trials
-	# print "LOWER TRIANG: \n" , make_lower_triangular(array(mat))
 
 	return [mat,trials]
 
@@ -184,17 +136,14 @@ def add_b_vec(mat):
 		mat[r].append(0)
 
 amat = get_all_linear_independents(128, True)[0]
-add_b_vec(amat)
 print amat
+add_b_vec(amat)
 amat = array(amat)
-amat = make_lower_triangular(amat)
+make_lower_triangular(amat)
 
 
 a = get_a(amat)
 print "A=", a
-#mat = array([[0 for x in range(5)] for y in range(5)])
-# avg_trials = avg_lin_trials(3)
-# print "AVG TRIALS: " , avg_trials
 
 
 
