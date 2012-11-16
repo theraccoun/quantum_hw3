f97420bd13fd3ee9a3f16c230c8a1bc821a9bc8e
Steven MacCoun <theraccoun@gmail.com>
2012-11-16 04:14:07 -0700

    Added some of the square exponentiation algorithm

--- modularExponentiation.py
+++ modularExponentiation.py
@@ -4,9 +4,57 @@ import math
 def modular_exponentiation(base, exponent, modulus):
 	c = 1
 	for e_prime in range(1, exponent+1):
-		print e_prime
 		c = (c * base) % modulus
 	return c
 
-print modular_exponentiation(1234, 1234*1234, math.pow(10, 10))
+def successive_squaring(base, exponent, modulus):
+	print exponent
+	e = 1
+	vals = {}
+	while e <= exponent:
+		sq = str(pow(base, e, modulus))
+		vals[e] = sq
+		print base , "^" , e, "    " , sq
+		e *= 2
+
+	print vals
+
+	ex = exponent
+	keys = sorted(vals)
+	print keys
+	
+	i = len(keys)-2
+	total = int(vals[keys[i]])
+	while ex > 1:
+		k = keys[i]
+		i -= 1
+		print "ex: " , ex
+		if ex - k < 0:
+			continue
+		else:
+			total = (total*int(vals[k])) % modulus
+			print "GOOD: " , ex
+			ex -= k
+
+	print total
+		# i = 0
+		# k = 0
+		# while i < len(keys):
+		# 	k = keys[i]
+		# 	if k > ex:
+		# 		k = keys[i-1]
+		# 		break
+		# 	i += 1
+
+		# print "ex: " , ex
+		# ex -= k
+		# if ex in keys:
+		# 	print ex
+		# 	total *= keys[ex]
+		# i += 1
+
+
+
+successive_squaring(1234, 1234*1234, int(math.pow(10,10)))
+# print modular_exponentiation(1234, 1234*1234, math.pow(10, 10))
 print pow(1234, 1234*1234, int(math.pow(10,10)))
